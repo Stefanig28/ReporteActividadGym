7353ecdfccc73fab034e842bda15240014e0d0ed
Stefani Torres <152030588+Stefanig28@users.noreply.github.com>
2024-07-11 10:40:34 -0500

    Add files via upload

---
+++ ReporteActividadGym/src/main/java/com/gestion/ReporteActividadGym/ReporteActividadGymApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.ReporteActividadGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporteActividadGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporteActividadGymApplication.class, args);
+	}
+
+}
