a93dd69a79b07256d875fbd0b63a58755ea2dfd8
Luis Antonio Bautista Cogollo <167202724+Luisbautista01@users.noreply.github.com>
2024-07-23 13:43:27 -0500

    Delete ReporteActividadGym directory

--- ReporteActividadGym/src/main/java/com/gestion/ReporteActividadGym/Excepcion/ActividadNoExistenteExcepcion.java
+++
@@ -1,7 +0,0 @@
-package com.gestion.ReporteActividadGym.Excepcion;
-
-public class ActividadNoExistenteExcepcion extends RuntimeException {
-    public ActividadNoExistenteExcepcion() {
-        super("No se han creado actividades.");
-    }
-}
--- ReporteActividadGym/src/main/java/com/gestion/ReporteActividadGym/Excepcion/AprendizNoEncontradoExcepcion.java
+++
@@ -1,7 +0,0 @@
-package com.gestion.ReporteActividadGym.Excepcion;
-
-public class AprendizNoEncontradoExcepcion extends RuntimeException {
-    public AprendizNoEncontradoExcepcion(Long aprendizId) {
-        super("El aprendiz " + aprendizId + " no se encuentra registrado.");
-    }
-}
