6f4f3bb2e052707c596b588b71972edb9a679055
Luis Antonio Bautista Cogollo <167202724+Luisbautista01@users.noreply.github.com>
2024-07-23 13:45:38 -0500

    Delete src directory

--- src/main/java/com/gestion/ReporteActividadGym/Excepcion/ActividadNoEncontradaExcepcion.java
+++
@@ -1,7 +0,0 @@
-package com.gestion.ReporteActividadGym.Excepcion;
-
-public class ActividadNoEncontradaExcepcion extends RuntimeException {
-    public ActividadNoEncontradaExcepcion(String id) {
-        super("La actividad " + id + " no se encuentra registrada.");
-    }
-}
