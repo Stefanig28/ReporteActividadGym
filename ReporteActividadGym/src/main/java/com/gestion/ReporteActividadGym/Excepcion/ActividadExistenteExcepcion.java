71ec5628bfac2ba2cd2da2ee8af80f264eb9f903
Luis Antonio Bautista Cogollo <167202724+Luisbautista01@users.noreply.github.com>
2024-07-23 13:27:51 -0500

    Delete ReporteActividadGym directory

--- ReporteActividadGym/src/main/java/com/gestion/ReporteActividadGym/Excepcion/ActividadExistenteExcepcion.java
+++
@@ -1,7 +0,0 @@
-package com.gestion.ReporteActividadGym.Excepcion;
-
-public class ActividadExistenteExcepcion extends RuntimeException {
-    public ActividadExistenteExcepcion(Long aprendizId) {
-        super("El aprendiz " + aprendizId + " ya ha registrado un entrenamiento para esta fecha con los mismos parámetros.");
-    }
-}
