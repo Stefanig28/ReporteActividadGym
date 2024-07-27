e047732f688a891971ffe048f9c1b07e0abd23ce
Luisbautista01 <bautistaluisantonio24@gmail.com>
2024-07-27 12:27:12 -0500

    [feature][task 9][9]

--- src/main/java/com/gestion/ReporteActividadGym/Configuracion/WebConfig.java
+++ src/main/java/com/gestion/ReporteActividadGym/Configuracion/WebConfig.java
@@ -7,8 +7,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081") // Adjust according to your needs
+                .allowedOrigins("https://reporteactividadgym-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
